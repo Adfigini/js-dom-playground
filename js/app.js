734fab285ba52ff89355410f58e3695e19409e7e
Alberto Figini <albertodfigini@gmail.com>
2026-01-05 23:34:27 -0300

    Add toggle state example with events

--- js/app.js
+++ js/app.js
@@ -6,3 +6,21 @@ button.addEventListener("click", () => {
   title.textContent = "Texto cambiado con JavaScript";
   description.textContent = "El DOM fue modificado correctamente 🚀";
 });
+
+const statusText = document.getElementById("statusText");
+const toggleBtn = document.getElementById("toggleBtn");
+
+let isActive = false;
+
+toggleBtn.addEventListener("click", () => {
+  isActive = !isActive;
+
+  if (isActive) {
+    statusText.textContent = "Estado actual: ON";
+    toggleBtn.textContent = "Desactivar";
+  } else {
+    statusText.textContent = "Estado actual: OFF";
+    toggleBtn.textContent = "Activar";
+  }
+});
+
